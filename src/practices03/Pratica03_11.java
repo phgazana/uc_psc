4f359d55f0ceeddd615e633c659889e85845d4b2
phgazana <phgazana@gmail.com>
2025-09-24 21:52:06 -0300

    init commit

---
+++ src/practices03/Pratica03_11.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class Pratica03_11 {
+    public static void main(String[] args) {
+
+        String consumidor;
+        int kWh;
+        double residencial, comercial, industrial;
+        double consumidorResidencial = 0, consumidorComercial = 0, consumidorIndustrial = 0, consumidorFinal = 0;
+        double media;
+
+        residencial = 0.3;
+        comercial = 0.5;
+        industrial = 0.7;
+
+        do {
+            consumidor = JOptionPane.showInputDialog("0 - Sair \n1- Residencial \n2- Comercial \n3- Industrial");
+
+            if (consumidor.equals("0")) {
+                break;
+            } else if (consumidor.equals("1")) {
+                kWh = Integer.parseInt(JOptionPane.showInputDialog("Quanto consumo(kWh):"));
+                consumidorFinal = kWh * residencial;
+                consumidorResidencial = consumidorFinal;
+            } else if (consumidor.equals("2")) {
+                kWh = Integer.parseInt(JOptionPane.showInputDialog("Quanto consumo(kWh):"));
+                consumidorFinal = kWh * comercial;
+                consumidorComercial = consumidorFinal;
+            } else if (consumidor.equals("3")) {
+                kWh = Integer.parseInt(JOptionPane.showInputDialog("Quanto consumo(kWh):"));
+                consumidorFinal = kWh * industrial;
+                consumidorIndustrial = consumidorFinal;
+            }
+
+            JOptionPane.showMessageDialog(null, "Valor: " + consumidorFinal);
+
+        } while (true);
+
+        if (consumidorResidencial + consumidorComercial > 0) {
+            media = (consumidorResidencial + consumidorComercial) / 2;
+        } else {
+            media = 0;
+        }
+
+        JOptionPane.showMessageDialog(null, "Residencial: " + consumidorResidencial + "\nComercial: " + consumidorComercial + "\nIndustrial: " + consumidorIndustrial);
+        JOptionPane.showMessageDialog(null, "Média: " + media);
+    }
+}
---
+++ src/practices03/Pratica03_26.java
@@ -0,0 +1,43 @@
+import javax.swing.JOptionPane;
+
+public class Pratica03_26 {
+    public static void main(String[] args) {
+
+        String operacao;
+        double num1, num2;
+        double resultado = 0;
+
+        do {
+            operacao = JOptionPane.showInputDialog("Operações Disponíveis: \n1. Adição \n2. Subtração  \n3. Multiplicação \n4. Divisão \n9. Sair do Programa \nDigite o número de ordem da opção desejada:");
+
+            switch (operacao) {
+                case "1":
+                    num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    resultado = num1 + num2;
+                    break;
+                case "2":
+                    num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    resultado = num1 - num2;
+                    break;
+                case "3":
+                    num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    resultado = num1 * num2;
+                    break;
+                case "4":
+                    num1 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    num2 = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
+                    resultado = num1 / num2;
+                    break;
+                case "9":
+                    break;
+            }
+            if (!operacao.equals("9")) {
+                JOptionPane.showMessageDialog(null, "Resultado: " + resultado);
+            }
+        } while (!operacao.equals("9"));
+    }
+}
+
