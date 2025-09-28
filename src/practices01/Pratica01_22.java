b6d18f45bdbcf65e65e049d5b8d9a1f5a7079ba5
phgazana <phgazana@gmail.com>
2025-09-28 11:11:33 -0300

    init commit

--- src/practices01/Pratica01_22.java
+++ src/practices01/Pratica01_22.java
@@ -3,14 +3,9 @@ import javax.swing.JOptionPane;
 public class Pratica01_22 {
     public static void main(String[] args) {
 
-        double numero, quadrado, raiz;
+        int num = Integer.parseInt(JOptionPane.showInputDialog("Digite um numero:"));
 
-        numero = Double.parseDouble(JOptionPane.showInputDialog("Digite um número:"));
-
-        quadrado = Math.pow(numero, 2);
-        raiz = Math.sqrt(numero);
-
-        JOptionPane.showMessageDialog(null, "Número: " + numero + "\n" + "Quadrado: " + quadrado + "\n" + "Raiz:" + raiz);
+        JOptionPane.showMessageDialog(null, "Numero: " + num + "\nQuadrado: " + Math.pow(num, 2) + "\nRaiz quadrada: " + Math.sqrt(num));
 
     }
 }
--- src/practices01/Pratica01_24.java
+++ src/practices01/Pratica01_24.java
@@ -3,12 +3,12 @@ import javax.swing.JOptionPane;
 public class Pratica01_24 {
     public static void main(String[] args) {
 
-        double area, diagonal_maior, diagonal_menor;
+        double area, diagonalMaior, diagonalMenor;
 
-        diagonal_maior = Double.parseDouble(JOptionPane.showInputDialog("Digite a diagonal maior:"));
-        diagonal_menor = Double.parseDouble(JOptionPane.showInputDialog("Digite a diagonal menor:"));
+        diagonalMaior = Double.parseDouble(JOptionPane.showInputDialog("Digite a diagonal maior:"));
+        diagonalMenor = Double.parseDouble(JOptionPane.showInputDialog("Digite a diagonal menor:"));
 
-        area = (diagonal_maior * diagonal_menor) / 2;
+        area = (diagonalMaior * diagonalMenor) / 2;
 
         JOptionPane.showMessageDialog(null, "A área do losango é: " + area);
     }
